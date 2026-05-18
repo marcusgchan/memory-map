0e299d273d49520f25693eca525871e95dec3ec2
Marcus Chan <marcuscchan@gmail.com>
2026-05-18 12:53:10 -0700

    test db process.env

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,8 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
+// import { env } from "./env";
 import * as schema from "./schema";
 
+console.log("env", process.env);
 const client = postgres(process.env.DATABASE_URL as unknown as string, {
   prepare: false,
 });
